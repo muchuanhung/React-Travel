f65a280fcce6065df358b47f43d20e9352eb753f
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2022-05-25 20:04:25 +0800

    fix ui import issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.module.css';
+import React from "react";
+import logo from "./logo.svg";
 
+import "./App.module.css";
 import styles from "./App.module.css";
+import { Layout, Typography, Input } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
-      還好嗎
+      <div>
+        <Layout.Header>
+          <img src={logo} alt="logo" />
+          <Typography.Title level={3}>Asia Travel</Typography.Title>
+          <Input.Search 
+            placeholder={"請输入旅游目的地、主题、或關鍵字"}
+            className={styles["search-input"]}
+          >
+          </Input.Search>
+        </Layout.Header>
+      </div>
     </div>
   );
 }
