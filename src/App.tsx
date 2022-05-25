b00eff25f1f6a7dd9c1861e00e88e67dd974d0bd
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2022-05-25 19:29:42 +0800

    fix: UI issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.module.css';
 
 import styles from "./App.module.css";
@@ -7,20 +6,7 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      還好嗎
     </div>
   );
 }
